13f1b5ce8f9d2e8485da077814093a1af42fc1ba
riusky <riuskyu@163.com>
2024-07-13 21:50:09 +0800

    生命周期

--- a02_HashMap/src/main.rs
+++ a02_HashMap/src/main.rs
@@ -43,4 +43,65 @@ fn main() {
     println!("因为过于无耻，{}已经被从帅气男孩名单中除名", name);
     println!("还有，他的真实年龄远远不止{}岁", age);
 
+
+    /* 查询 HashMap */
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    let team_name = String::from("Blue");
+    let score: Option<&i32> = scores.get(&team_name);   // 注意这里返回的是一个Option<&i32> &i32 可以看出返回的是一个借用值
+    // get 方法返回一个 Option<&i32> 类型：当查询不到时，会返回一个 None，查询到时返回 Some(&i32)
+    // &i32 是对 HashMap 中值的借用，如果不使用借用，可能会发生所有权的转移
+    let score: i32 = scores.get(&team_name).copied().unwrap_or(0);  //直接获取值
+
+    //  Option::copied
+    // copied方法用于将包含引用的Option转换为包含值的Option。
+    // 它仅在Option包含一个可以复制的引用时有效。
+    // 例如，如果你有一个Option<&T>，你可以使用copied将其转换为一个Option<T>，前提是T实现了Copy trait。
+    let x: Option<&i32> = Some(&10);
+    let copied_x: Option<i32> = x.copied();
+    println!("{:?}", copied_x); // 输出: Some(10)
+    // 在这个示例中，copied方法将Option<&i32>转换为了Option<i32>。
+
+    // Option::unwrap_or
+    // unwrap_or方法用于在Option是Some时返回其中的值，而在Option是None时返回提供的默认值。
+    let x: Option<i32> = Some(10);
+    let y: Option<i32> = None;
+    let value1 = x.unwrap_or(5);
+    let value2 = y.unwrap_or(5);
+    println!("{}", value1); // 输出: 10
+    println!("{}", value2); // 输出: 5
+
+
+    /* 更新 HashMap 中的值 */
+    let mut scores = HashMap::new();
+    scores.insert("Blue", 10);
+    // 覆盖已有的值
+    let old = scores.insert("Blue", 20);
+    assert_eq!(old, Some(10));
+
+    // 查询新插入的值
+    let new = scores.get("Blue");
+    assert_eq!(new, Some(&20));
+
+    // 查询Yellow对应的值，若不存在则插入新值
+    let v = scores.entry("Yellow").or_insert(5);
+    assert_eq!(*v, 5); // 不存在，插入5
+
+    // 查询Yellow对应的值，若不存在则插入新值
+    let v = scores.entry("Yellow").or_insert(50);
+    assert_eq!(*v, 5); // 已经存在，因此50没有插入
+
+    // 在已有值的基础上更新 |- 全局变量  |- 临时缓存
+    let text = "hello world wonderful world";
+    // 在文本中统计词语出现的次数
+    let mut map = HashMap::new();
+    // 根据空格来切分字符串(英文单词都是通过空格切分)
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
 }
---
+++ a03_life_cryle/src/main.rs
@@ -0,0 +1,94 @@
+fn main() {
+    println!("Hello, world!");
+
+    ///
+    /// Rust的生命周期（lifetime）是Rust的借用检查器用于确保引用在使用时有效的一个重要概念。
+    /// 它帮助Rust在编译时确保内存安全，防止悬空引用和数据竞争。
+    /// 理解生命周期对编写安全且高效的Rust代码非常关键。
+    /// 以下是生命周期的详细介绍：
+    ///
+    /// *生命周期的基本概念*
+    /// 生命周期是Rust编译器用来跟踪引用如何在程序中存活的一种方式。
+    /// 每个引用都有一个生命周期，它表示引用在程序中保持有效的范围。
+    /// Rust在编译时通过生命周期来确保所有引用在它们被使用时都是有效的。
+    ///
+    /// *生命周期标注*
+    /// 生命周期标注用单引号（'）后跟一个名字表示，如'a。
+    /// 生命周期标注通常出现在函数签名中，以表示输入和输出引用的关系。
+    /// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    //     if x.len() > y.len() {
+    //         x
+    //     } else {
+    //         y
+    //     }
+    //  }
+    ///
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
+
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+    println!("The longest string is {}", string1);
+
+
+
+
+    /// 下面的错误可以考虑其他方式解决  move等
+    // let string1 = String::from("long string is long");
+    // let result;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {}", result);
+
+
+    // 函数的返回值如果是一个引用类型，那么它的生命周期只会来源于：
+    // 函数参数的生命周期
+    // 函数体中某个新建引用的生命周期 若是后者情况，就是典型的悬垂引用场景：
+    /// fn longest<'a>(x: &str, y: &str) -> &'a str {
+    //     let result = String::from("really long string");
+    //     result.as_str()
+    // }
+    /// 那遇到这种情况该怎么办？最好的办法就是返回内部字符串的所有权，然后把字符串的所有权转移给调用者：
+    fn longest_t<'a>(_x: &str, _y: &str) -> String {
+        String::from("really long string")
+    }
+
+    let s = longest_t("not", "important");
+}
+
+///struct ImportantExcerpt {
+//     part: &str,
+// }
+// struct ImportantExcerpt<'a> {
+//     part: &'a str,
+// }
+
+/* 每一个引用参数都会获得独自的生命周期 */
+/* 若只有一个输入生命周期(函数参数中只有一个引用类型)，那么该生命周期会被赋给所有的输出生命周期 */
+/* 若存在多个输入生命周期，且其中一个是 &self 或 &mut self，则 &self 的生命周期被赋给所有的输出生命周期 */
+
+/* 结构体中的生命周期实现方法注意 */
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+// impl 中必须使用结构体的完整名称，包括 <'a>，因为生命周期标注也是结构体类型的一部分！
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
